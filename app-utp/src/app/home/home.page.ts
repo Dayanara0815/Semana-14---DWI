1f89c9b484914ded59ad748d81fea2a712860d09
Dayanara Caro <dayanaracarorojas@gmail.com>
2026-06-23 14:24:37 +0000

    Semana 11 de app base Ionic Angular con formulario

---
+++ app-utp/src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  enviado = false;
+  registroForm = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    correo: ['', [Validators.required, Validators.email]],
+    tipoApp: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) { }
+  get f() { return this.registroForm.controls; }
+
+  validarRegistro() {
+    this.enviado = true;
+    if (this.registroForm.invalid) {
+      this.registroForm.markAllAsTouched();
+      return;
+    }
+    console.log('Registro válido',
+      this.registroForm.value);
+  }
+
+}
---
+++ app-utp/src/app/models/registro.ts
@@ -0,0 +1,13 @@
+export interface Registro {
+ id?: number;
+ nombre: string;
+ correo: string;
+ tipoApp: string;
+ fecha?: string;
+}
+
+export interface ApiRespuesta {
+ ok: boolean;
+ mensaje: string;
+ data?: Registro;
+}
